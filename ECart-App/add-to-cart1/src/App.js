6a2596e6de99ab827639ba3d26f7e8d0fda4d3b9
Sushmitha27K <sushmitha2721.k@gmail.com>
2024-06-07 16:27:12 +0530

    Add files via upload

---
+++ ECart-App/add-to-cart1/src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import Headers from './components/Headers';
+import Home from './components/Home';
+import CartDetails from './components/CartDetails';
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <>
+    <Provider store={store}>
+      <Router>
+      <Headers/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<CartDetails/>}/>
+      </Routes>
+      </Router>
+    </Provider>
+    </>
+  );
+}
+
+export default App;
---
+++ ECart-App/add-to-cart1/src/components/CardData.js
@@ -0,0 +1,126 @@
+
+const CardsData = [
+    {
+        "id": 1,
+        "title": "Backpack",
+        "price": 19.95,
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "category": "men's clothing",
+        "image":"https://th.bing.com/th/id/OIG1.cZEd3TAYOkUjbGKYl952?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
+        "rating": {
+            "rate": 3.9,
+            "count": 120
+        }
+    },
+    {
+        "id": 2,
+        "title": "Mens T-Shirt",
+        "price": 22.3,
+        "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+        "category": "men's clothing",
+        "image": "https://th.bing.com/th/id/OIG1.YMsagYzqLfSM5oFqDExw?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
+        "rating": {
+            "rate": 4.1,
+            "count": 259
+        }
+    },
+    {
+        "id": 3,
+        "title": "Jeans Jacket",
+        "price": 55.99,
+        "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+        "category": "men's clothing",
+        "image": "https://th.bing.com/th/id/OIG2.LSYZ02V0CIAO8NHAiV01?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
+        "rating": {
+            "rate": 4.7,
+            "count": 500
+        }
+    },
+    {
+        "id": 4,
+        "title": "Mens Casual Slim Fit",
+        "price": 15.99,
+        "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+        "category": "men's clothing",
+        "image": "https://th.bing.com/th/id/OIG3.WA7du.STCtDIPaZJt_5Z?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
+        "rating": {
+            "rate": 2.1,
+            "count": 430
+        }
+    },
+    {
+        "id": 5,
+        "title": "Silver Dragon Bracelet",
+        "price": 100,
+        "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+        "category": "jewelery",
+        "image": "https://th.bing.com/th/id/OIG3.eBXNHlrqCje5hlEi_fff?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
+        "rating": {
+            "rate": 4.6,
+            "count": 400
+        }
+    },
+    {
+        "id": 6,
+        "title": "Ring",
+        "price": 698,
+        "description": "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+        "category": "jewelery",
+        "image": "https://th.bing.com/th/id/OIG3.4HyS9DyEXIGpnCsqtHH0?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
+        "rating": {
+            "rate": 3.9,
+            "count": 70
+        }
+    },
+    {
+        "id": 7,
+        "title": "Plated Chain",
+        "price": 19.98,
+        "description": "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
+        "category": "jewelery",
+        "image": "https://th.bing.com/th/id/OIG4.UCwWimjY4coUAJv9Omuv?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
+        "rating": {
+            "rate": 3,
+            "count": 400
+        }
+    },
+    {
+        "id": 8,
+        "title": "Steel Earrings",
+        "price": 10.64,
+        "description": "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
+        "category": "jewelery",
+        "image": "https://th.bing.com/th/id/OIG2.liqk3lyXDjVQFIyB5BLr?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
+        "rating": {
+            "rate": 1.9,
+            "count": 100
+        }
+    },
+    {
+        "id": 9,
+        "title": "Portable Hard Drive 1TB",
+        "price": 1.64,
+        "description": "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
+        "category": "electronics",
+        "image": "https://th.bing.com/th/id/OIG3.U6Q4GjcQeqOcWYmAB3j6?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
+        "rating": {
+            "rate": 3.3,
+            "count": 203
+        }
+    },
+    {
+        "id": 10,
+        "title": "SanDisk SSD PLUS 1TB",
+        "price": 10.89,
+        "description": "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        "category": "electronics",
+        "image": "https://th.bing.com/th/id/OIG2.4zwzce1BJWD85CNRA2Jv?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
+        "rating": {
+            "rate": 2.9,
+            "count": 470
+        }
+    },
+    
+];
+
+export default CardsData;
